17dee63b793fed4937bddbb21516ca7d4b7fa3f6
Ilton Andrew <iltonandrew@gmail.com>
2022-03-09 03:44:46 -0300

    fix: api headers

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,62 +1,58 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from 'nookies'
-import Router from 'next/router'
+import { setCookie, parseCookies } from "nookies";
+import Router from "next/router";
 
 import { logIn, recoverUserInfo } from "../services/auth";
 import { api } from "../services/api";
 import { User } from "public/model/User";
 
 type AuthContextType = {
-    user: User| null;
-    isAuthenticated: boolean;
-    signIn: (data: CredentialsType) => Promise<void>
-}
+  user: User | null;
+  isAuthenticated: boolean;
+  signIn: (data: CredentialsType) => Promise<void>;
+};
 
 type CredentialsType = {
-    cpf: string,
-    password: string
-}
+  cpf: string;
+  password: string;
+};
 
-export const AuthContext = createContext({} as AuthContextType)
+export const AuthContext = createContext({} as AuthContextType);
 
 type AuthProviderProps = {
-    children?: ReactNode
-}
+  children?: ReactNode;
+};
 
-export function AuthProvider({ children  }: AuthProviderProps) {
-    const [user, setUser] = useState<User | null>(null)
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
 
-    const isAuthenticated = !!user;
+  const isAuthenticated = !!user;
 
-    useEffect(() => {
-        const { token } = parseCookies()
+  useEffect(() => {
+    const { token } = parseCookies();
 
-        if(token) {
-            recoverUserInfo(token).then(response => {
-                setUser(response.user)
-            })
-        }
-    }, [])
-
-    async function signIn({ cpf, password }: CredentialsType) {
-        const { token, user } = await logIn({
-            cpf,
-            password
-        })
-
-        setCookie(undefined, 'token', token, {
-            maxAge: 60 *60 * 1, // 1hour
-        })
-
-        api.defaults.headers['Authorization'] = `Bearer ${token}`;
-
-        setUser(user)
-        Router.push('/dashboard')
+    if (token) {
+      recoverUserInfo(token).then((response) => {
+        setUser(response.user);
+      });
     }
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  async function signIn({ cpf, password }: CredentialsType) {
+    const { token, user } = await logIn({
+      cpf,
+      password,
+    });
+
+    setCookie(undefined, "token", token, {
+      maxAge: 60 * 60 * 1, // 1hour
+    });
+
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+    setUser(user);
+    Router.push("/dashboard");
+  }
+
+  return <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>{children}</AuthContext.Provider>;
 }
--- services/api.ts
+++ services/api.ts
@@ -1,20 +1,19 @@
-import axios from "axios"
-import { BASE_URL, BASE_URL_LOCAL } from "../pages/api/constants"
-import { parseCookies } from 'nookies';
+import axios from "axios";
+import { BASE_URL, BASE_URL_LOCAL } from "../pages/api/constants";
+import { parseCookies } from "nookies";
 
 export const api = Api();
 
 export function Api(ctx?: any) {
-    const { token } = parseCookies(ctx);
+  const { token } = parseCookies(ctx);
 
-    const api = axios.create({
-        baseURL: BASE_URL
-    })
+  const api = axios.create({
+    baseURL: BASE_URL,
+  });
 
-    if (token) {
-        api.defaults.headers['Authorization'] = `Bearer ${token}`;
-    }
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
 
-    return api;
+  return api;
 }
-
