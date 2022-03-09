7df109a1e0fd4c191d84cf5d7d82c2668cedcf71
Ilton Andrew <iltonandrew@gmail.com>
2022-03-09 03:46:46 -0300

    fix: api urls

--- services/auth.ts
+++ services/auth.ts
@@ -1,34 +1,35 @@
-import {BASE_URL, BASE_URL_DEV} from '../pages/api/constants'
-import { api } from './api';
+import { BASE_URL } from "../pages/api/constants";
+import { api } from "./api";
 
 type CredentialsType = {
-    cpf: string,
-    password: string
-}
-
-export async function logIn({cpf, password}:CredentialsType) {
-
-  return api.post('/auth', {cpf, password})
-  .then(function (response) {
-    return response.data;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  cpf: string;
+  password: string;
 };
 
+export async function logIn({ cpf, password }: CredentialsType) {
+  return api
+    .post("/auth", { cpf, password })
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 export async function recoverUserInfo(token: string) {
-  return api.get('/user/recoverInfo', {})
-  .then(function (response) {
-    return response.data;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  return api
+    .get("/user/recoverInfo", {})
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 export async function logOut() {
-  localStorage.removeItem('token')
+  localStorage.removeItem("token");
   // redirect to '/'
   return;
 }
