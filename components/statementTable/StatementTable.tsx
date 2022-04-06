f1e7be40ee0c6435148baf0e00461661a7455a0e
joao pedro <jps.cortez@hotmail.com>
2022-04-06 14:20:43 -0300

    added charts to other pages

---
+++ components/statementTable/StatementTable.tsx
@@ -0,0 +1,66 @@
+import { Table, Thead, Tr, Th, Tbody, Flex, Divider, IconButton, useBoolean, Heading, Text } from "@chakra-ui/react";
+import { FinanceDataType } from "public/model/FinanceData";
+import { MouseEventHandler } from "react";
+import { FiRefreshCcw, FiChevronDown, FiChevronUp } from "react-icons/fi";
+import StatementItem from "./StatementItem";
+
+type StatementTableType = {
+    title: string;
+    lastUpdate: Date;
+    enableRefresh?: boolean;
+    statements: FinanceDataType[];
+    onRefreshClicked?: MouseEventHandler<HTMLButtonElement> | undefined
+}
+
+export default function StatementTable(props: StatementTableType) {
+
+  const [ display, toggleDisplay ] = useBoolean(false)
+
+  return (
+
+    <Flex flexDir="column">
+      {/* Title */}
+      <Flex justifyContent="space-between" mt={8}>
+          <Flex align="flex-end">
+            <Heading as="h2" size="lg" letterSpacing="tight">
+              {props.title}
+            </Heading>
+            <Text fontSize="small" color="gray" ml={4}>
+              Updated at {new Date(props.lastUpdate).toLocaleString()}
+            </Text>
+          </Flex>
+          <IconButton icon={<FiRefreshCcw />} onClick={props.onRefreshClicked} aria-label="aa" />
+        </Flex>
+      {/* Table */}
+      <Flex overflow="auto">
+        <Table variant="unstyled" mt={4}>
+          <Thead>
+            <Tr color="gray">
+              <Th>Establishment</Th>
+              <Th>Category</Th>
+              <Th isNumeric>Money Spent</Th>
+              <Th>Bank</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {(display ? props.statements : props.statements.slice(0, 4)).map((statement) => {
+              return (
+                <StatementItem statement={statement}></StatementItem>
+              )
+            })}
+          </Tbody>
+        </Table>
+      </Flex>
+      {/* Toggle button */}
+      <Flex align="center">
+        <Divider />
+        <IconButton
+          icon={display ? <FiChevronUp /> : <FiChevronDown />}
+          onClick={toggleDisplay.toggle}
+          aria-label="aaa"
+        />
+        <Divider />
+      </Flex>
+    </Flex>
+  )
+}
--- pages/statement/index.tsx
+++ pages/statement/index.tsx
@@ -1,12 +1,83 @@
+import { Flex } from "@chakra-ui/react";
+import MultipleLineChart from "components/MultipleLineChart";
 import Content from "components/PageComponents/content/Content";
 import LoggedPageContainer from "components/PageComponents/LoggedPageContainer";
 import Sidebar from "components/PageComponents/sidebar/Sidebar";
+import StatementTable from "components/statementTable/StatementTable";
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
+import { FinanceDataType } from "public/model/FinanceData";
+import { Api } from "services/api";
+import { format } from "date-fns";
 
-export default function StatementPage() {
+type StatementPagePropsType = {
+    timestamp: Date;
+    financeDataItems: FinanceDataType[];
+  };
+
+export default function StatementPage(props: StatementPagePropsType) {
+
+    const lineChartData: {[id: string]: number[]} = {};
+
+    // generate line chart data
+    const currentMonth: number = new Date().getMonth() + 1;
+    
+    props.financeDataItems.forEach((item: FinanceDataType) => {
+        if(!(item.originInstitution in lineChartData)) 
+            lineChartData[item.originInstitution] = Array(currentMonth).fill(0)
+        
+        const month: number = new Date(item.referenceDate).getMonth()
+        lineChartData[item.originInstitution][month] += item.value
+    })
+
     return (
         <LoggedPageContainer>
             <Sidebar></Sidebar>
-            <Content title="Extrato"></Content>
+            <Content title="Extrato">
+                <Flex height='50vh'>
+                    <MultipleLineChart data={lineChartData}/>
+                </Flex>
+                <StatementTable title="Extrato" statements={props.financeDataItems} lastUpdate={props.timestamp}></StatementTable>
+            </Content>
         </LoggedPageContainer>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const apiClient = Api(ctx);
+
+  return apiClient
+    .get("/financeData")
+    .then((res) => {
+      let timestamp: Date;
+      let financeDataItems: FinanceDataType[];
+
+      if (!res.data) {
+        return {
+          props: { financeDataItems: [] },
+        };
+      }
+
+      financeDataItems = res.data.financeDataItems as FinanceDataType[];
+      timestamp = res.data.timestamp as Date;
+
+      return {
+        props: { timestamp, financeDataItems },
+      };
+    })
+    .catch(() => {
+      return {
+        props: {},
+      };
+    });
+};
